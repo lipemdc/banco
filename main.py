efa5576e414981c6c25f9b0a75be8f2e60a3330a
lipemdc <luizfelipe201056@gmail.com>
2025-02-23 01:24:39 -0300

    banco 1.2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             if nome in contas:
                 raise ValueError("ja existe uma conta com esse nome")
             
-            n = input("digite 1 para conta corrente\ndigite 2 para conta poupança")
+            n = input("digite 1 para conta corrente\ndigite 2 para conta poupança ")
             if n == "1":
                 contas[nome] = ContaCorrente(nome)
                 tipo = "corrente"
@@ -45,28 +45,48 @@ if __name__ == "__main__":
             nome = input("digite o nome da conta ")
             if nome not in contas:
                 raise ValueError("essa conta não existe")
-            valor = float(input("digite o valor a ser sacado."))
+            valor = float(input("digite o valor a ser sacado. "))
             if valor <= 0:
                 raise ValueError("o valor a ser sacado deve ser maior que 0")
             
-            saque = contas[nome].sacar(valor)
-            if saque == False:
+            if not contas[nome].sacar(valor):
                 raise ValueError("saldo insuficiente para saque")
             else:
                 print(f"saque de R${valor} realizado na conta {nome}")
 
         except ValueError as e:
-            print(f"erro:{e}")
+            print(f"erro: {e}")
+
 
     def verificar_saldo():
-        nome = input("digite o nome da conta")
+        
+        nome = input("digite o nome da conta ")
         if nome not in contas:
             raise ValueError("essa conta não existe")
         print(contas[nome])
 
+    def transferir():
+        try:
+            nome1 = input("digite a conta de origem ")
+            nome2 = input("digite a conta destino ")
+            if nome1 not in contas or nome2 not in contas:
+                raise ValueError("uma ou as duas contas não existem")
+            valor = float(input("digite o valor a ser transferido "))
+            if valor <= 0:
+                raise ValueError("o valor a ser transferido deve ser maior que 0")
+            
+            if not contas[nome1].transferencia(valor,contas[nome2]):
+                raise ValueError("saldo insuficiente para a transferência")
+            else:
+                print(f"pix de R${valor} realizado de {nome1} para {nome2}")
+
+        except ValueError as e:
+            print(f"erro: {e}")
+        
+
     def menu():
         while True:
-            print("---BANCO LIPE---\n1- criar conta\n2- depositar\n3- sacar\n4- verificar saldo\n5- sair")
+            print("---BANCO LIPE---\n1- criar conta\n2- depositar\n3- sacar\n4- verificar saldo\n5- pix\n6- sair")
             try:    
                 escolha = input("oque deseja fazer?")
                 if escolha == "1":
@@ -78,12 +98,12 @@ if __name__ == "__main__":
                 elif escolha == "4":
                     verificar_saldo()
                 elif escolha == "5":
+                    transferir()
+                elif escolha == "6":
                     print("---Até a próxima, volte sempre!!---")
                     break
                 else: 
-                    raise ValueError("digite um numero de 1 a 4")
+                    raise ValueError("digite um numero de 1 a 6")
             except ValueError as e:
                 print(f"erro: {e}")
-
-
     menu()
